6f58c86fb24b384a60b2bf38963f7651067d90ef
Sawyer Labanowski <labanowskisawyer@gmail.com>
2019-05-13 23:12:31 -0500

    Initialized project. Wrote editor script so objects will snap to grid.

---
+++ Assets/Scripts/EditModeScripts/EditorSnap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Tooltip("The size of each grid space an object will snap to.")]
+    [Range(1, 20)]
+    [SerializeField] int gridSize = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+    }
+}
