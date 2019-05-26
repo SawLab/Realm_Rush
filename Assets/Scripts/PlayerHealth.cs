acaf759dd393f6cdfaf9b0a0d3d0ad6f2af71088
Sawyer Labanowski <labanowskisawyer@gmail.com>
2019-05-26 17:01:15 -0500

    Rearranged level. Enemies reaching endpoint now decrease player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int playerHealth = 20;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (--playerHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
