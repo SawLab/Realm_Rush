2322a4f9a3002d70cc5c2eedfd845e47f46ee7ec
Sawyer Labanowski <labanowskisawyer@gmail.com>
2019-05-27 23:56:01 -0500

    Commit

--- Assets/Scripts/TowerFactory.cs
+++ Assets/Scripts/TowerFactory.cs
@@ -27,24 +27,20 @@ public class TowerFactory : MonoBehaviour
         oldTower.baseWaypoint.isPlaceable = true;
         oldTower.baseWaypoint = waypoint;
         waypoint.isPlaceable = false;
-        Vector3 position = GetNewPosition(waypoint);
+        MeshRenderer topMeshRenderer = waypoint.transform.Find("Top").GetComponent<MeshRenderer>();
+        Vector3 position = topMeshRenderer.transform.position;
         oldTower.transform.position = position;
         towerQueue.Enqueue(oldTower);
     }
 
     private void InstantiateNewTower(Waypoint waypoint)
     {
-        Vector3 position = GetNewPosition(waypoint);
+        MeshRenderer topMeshRenderer = waypoint.transform.Find("Top").GetComponent<MeshRenderer>();
+        Vector3 position = topMeshRenderer.transform.position;
         Tower newTower = Instantiate(towerToPlace, position, Quaternion.identity);
         waypoint.isPlaceable = false;
         newTower.baseWaypoint = waypoint;
         newTower.transform.parent = towerParent.transform;
         towerQueue.Enqueue(newTower);
-    }
-
-    private Vector3 GetNewPosition(Waypoint waypoint)
-    {
-        MeshRenderer topMeshRenderer = waypoint.transform.Find("Top").GetComponent<MeshRenderer>();
-        return topMeshRenderer.transform.position;
     }
 }
